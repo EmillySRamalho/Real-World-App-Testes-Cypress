b99b2369076a4da6d9fadc8b9c3cc6220a46b89e
EmillySRamalho <emilly_bsfpb@hotmail.com>
2025-12-08 16:19:55 -0300

    avançando nos testes cypress

--- cypress/e2e/signin.cy.js
+++ cypress/e2e/signin.cy.js
@@ -1,28 +1,40 @@
-describe('template spec', () => {
+describe('Fluxo completo de cadastro, login e onboarding', () => {
+  it('Criar conta, fazer login e completar onboarding uma vez', () => {
 
-  beforeEach(() => {
     cy.visit('http://localhost:3000/signin')
-  })
+
   
-  it ( 'criar conta de usuario', () => {
-     cy.contains('Sign Up').click()
-     cy.get('#firstName').type('Katharina')
-     cy.get('#lastName').type('Bernier')
-     cy.get('#username').type('Katharina_Bernier')
-     cy.get('#password').type('s3cret')
-     cy.get('#confirmPassword').type('s3cret')
-     cy.get('button[type=submit]').click()
+    cy.contains('Sign Up').click()
+    cy.get('#firstName').type('Katharina')
+    cy.get('#lastName').type('Bernier')
+    cy.get('#username').type('Katharina_Bernier')
+    cy.get('#password').type('s3cret')
+    cy.get('#confirmPassword').type('s3cret')
+    cy.get('button[type=submit]').click()
 
 
-  })
-
-  it('inserir dados do usuario', () => {
     cy.get('#username').type('Katharina_Bernier')
     cy.get('#password').type('s3cret')
     cy.get('input[type=checkbox]').click()
     cy.get('button[type=submit]').click()
-  
+
+    cy.get('body').then(($body) => {
+      if ($body.find('[data-test="user-onboarding-next"]').length > 0) {
+        cy.get('[data-test="user-onboarding-next"]').click()
+      }
+    })
+
+    cy.get('body').then(($body) => {
+      if ($body.find('#bankaccount-bankName-input').length > 0) {
+
+        cy.get('#bankaccount-bankName-input').type('WillBank')
+        cy.get('#bankaccount-routingNumber-input').type('858805148')
+        cy.get('#bankaccount-accountNumber-input').type('877444999')
+        cy.get('button[type=submit]').click()
+
+        cy.get('[data-test="user-onboarding-next"]').click()
+      }
+    })
 
   })
 })
-
