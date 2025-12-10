e4607335617ce267a100400e3c1cb1b49d327d88
EmillySRamalho <emilly_bsfpb@hotmail.com>
2025-12-09 21:54:44 -0300

    Atualizaçoes de dados cadastrais

---
+++ cypress/e2e/AtualizarDadosCadastrais.cy.js
@@ -0,0 +1,16 @@
+describe('Atualizar dados cadastrais', () => {
+     it ("clicar em realizar nova transacao", ()  => { 
+      cy.visit('http://localhost:3000')
+      cy.get('#username').type('Katharina_Bernier')
+      cy.get('#password').type('s3cret')
+      cy.get('input[type=checkbox]').click()
+      cy.get('button[type=submit]').click()
+
+      cy.contains('span', 'My Account').click()
+      cy.get("#user-settings-email-input").type('Berner_Kath@teste.com')
+      cy.get('#user-settings-phoneNumber-input').type('987456321')
+
+       cy.get('[data-test="user-settings-submit"]').click()
+      
+     })
+    })
