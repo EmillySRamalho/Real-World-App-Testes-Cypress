0437873395730807090f39134541a5d8147ea585
EmillySRamalho <emilly_bsfpb@hotmail.com>
2025-12-11 16:57:20 -0300

    testes de filtragem

--- cypress/e2e/Filtrar.cy.js
+++ cypress/e2e/Filtrar.cy.js
@@ -17,6 +17,22 @@ describe('Atualizar dados cadastrais', () => {
 
       cy.get('[data-test="transaction-list-filter-date-clear-button"]').click({ force: true }) 
 
+      cy.contains('span', 'Amount: $0 - $1,000').click({force:true})
+
+      cy.get('[data-test="transaction-list-filter-amount-range-slider"] input')
+            .eq(0)
+            .invoke('val', 15.5)
+            .trigger('change', { force: true });
+
+        cy.get('[data-test="transaction-list-filter-amount-range-slider"] input')
+            .eq(0)
+            .click({ force: true });
+
+            cy.wait(1000)
+
+       cy.get('[data-test="transaction-list-filter-amount-clear-button"]').click({force:true})
+
+
 
 
 
